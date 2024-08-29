1256e71352f65dd65ddcd136fefdd9fa3dd4a791
rupali-for-chegg <rgumaste@indeed.com>
2024-08-29 20:44:35 +0900

    updated routers to fix failing routes

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -2,16 +2,25 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// Reviews routes
+// Route to handle all reviews
+router
+    .route("/")
+    .get(controller.listReviewsForMovie)
+    .all(methodNotAllowed);
+
+
+// Routes for specific review operations
 router
     .route("/:reviewId")
     .delete(controller.destroy)
     .put(controller.update)
     .all(methodNotAllowed);
-router
-    .route("/")
-    .get(controller.listReviewsForMovie)
-    .get(controller.list)
-    .all(methodNotAllowed);
 
+// Routes for specific review operations
+router
+    .route("/reviews")
+    .get(controller.list)
+    .delete(controller.destroy)
+    .put(controller.update)
+    .all(methodNotAllowed);
 module.exports = router;
