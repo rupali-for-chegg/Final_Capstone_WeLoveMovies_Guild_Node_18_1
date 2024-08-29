db319431116d8fcf245bb0d5656514b701b8e55d
rupali-for-chegg <167495051+rupali-for-chegg@users.noreply.github.com>
2024-08-29 20:47:09 +0900

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -5,14 +5,9 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 // Route to handle all reviews
 router
     .route("/")
-    .get(controller.listReviewsForMovie)  // List all reviews
+    .get(controller.listReviewsForMovie)
     .all(methodNotAllowed);
 
-// Route to handle reviews for a specific movie
-router
-    .route("/movies/:movieId/reviews")
-    .get(controller.listReviewsForMovie) // List reviews for a specific movie
-    .all(methodNotAllowed);
 
 // Routes for specific review operations
 router
@@ -21,4 +16,11 @@ router
     .put(controller.update)
     .all(methodNotAllowed);
 
+// Routes for specific review operations
+router
+    .route("/reviews")
+    .get(controller.list)
+    .delete(controller.destroy)
+    .put(controller.update)
+    .all(methodNotAllowed);
 module.exports = router;
